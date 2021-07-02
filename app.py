cc643d689f5da85360cd5c54d8541d1daf0d3797
Seth <60856766+sethupavan12@users.noreply.github.com>
2021-07-02 04:19:21 +0100

    2nd Stream
    - Partially working decryption
    - encryption works alright

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 UPLOAD_FOLDER = 'static/uploads/'
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = 'decrypted_images/'
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import os
 from app import app
+
 import urllib.request
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template,session
+from flask_wtf import Form
+from wtforms import IntegerField, StringField, SubmitField
 from werkzeug.utils import secure_filename
+from wtforms.validators import DataRequired
 from PIL import Image
 from random import randint
 import numpy
 import sys
 from helper import *
 from encrypt import encrypt_image
+from decrypt import decrypt_image
+
+class InputForm(Form):
+    Kc = IntegerField(label='Kc',validators=[DataRequired()])
+    Kr = IntegerField(label='Kr',validators=[DataRequired()])
+    ITER_MAX = IntegerField(label='ITER_MAX',validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
@@ -57,7 +70,45 @@ def upload_image():
 
 @app.route('/decrypt_image')  #open the decrypt page and upload the 'shaky' pic to get clear pic 
 def decrypt_image():
-    render_template('decrypt.html')
+    form = InputForm()
+    return render_template('decrypt.html',form=form)
+    
+
+
+
+@app.route('/decrypt_image',methods=['POST'])
+def post_decrypt_image():
+    if 'file' not in request.files:
+        flash('No file found')
+        return redirect(request.url)
+
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        full_filename=os.path.join(app.config['DOWNLOAD_FOLDER'], filename) #Upload path decrypted_images/filename.png
+
+        file.save(full_filename)
+        #print('upload_image filename: ' + filename)
+
+        form = InputForm()
+        if form.validate_on_submit:
+            #get values from a form
+            Kc = request.form.getlist('Kc', type=int)
+            Kr = request.form.getlist('Kr', type=int)
+            ITER_MAX = request.form('ITER_MAX', type=int)
+
+
+            decrypt_image(full_filename,Kc,Kr,ITER_MAX)
+            flash('image decrypted')
+            pathy='decrypted_images/decrypt.png'
+            return render_template('decrypt.html',pathy=pathy,full_filename=full_filename,form=form)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
 
 
 
@@ -77,12 +128,20 @@ def parse_attributes_into_txt(kc,kr,ITER_MAX): #take list as input
 
 
 
-def parse_txt_into_list(path_with_filename): #str input 
-	#my_file = open(path_with_filename, "r")
-	#content = my_file.read()
-	pass
+def parse_txt_into_list(path_with_filename): #str input   #NO NEED TO USE NOW
+    #my_file = open(path_with_filename, "r")
+    content=[]
+    with open(path_with_filename, 'r'):
+        for i in path_with_filename:
+            content.append(i)
+
+    return content
+
 
 
 
 if __name__ == "__main__":
     app.run()
+
+#THINGS TO DO 
+# ADD DOWNLOAD BUTTON FOR THE IMAGE
